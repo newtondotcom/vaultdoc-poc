a524b94962f47429e7fb81c94d0eb003ed1a4f38
newtondotcom <asphalt9fr@gmail.com>
2024-02-05 23:19:12 +0100

    button to open modal to choose adding method

--- src/ui/Drawer.tsx
+++ src/ui/Drawer.tsx
@@ -1,19 +1,39 @@
 import { DrawerContentScrollView, DrawerItem, DrawerItemList } from "@react-navigation/drawer";
-import { Linking, Text, View } from "react-native";
+import { Linking, StyleSheet, Text, View } from "react-native";
 
-function Drawer(props: any) {
-    return (
-        <DrawerContentScrollView {...props}>
-            <View style={{ height: 150, backgroundColor: 'lightgrey', justifyContent: 'center', alignItems: 'center' }}>
-                <Text>My App</Text>
-            </View>
+/*
             <DrawerItem
                 label="Help"
                 onPress={() => Linking.openURL('https://mywebsite.com/help')}
             />
+*/
+
+function Drawer(props: any) {
+    return (
+        <DrawerContentScrollView {...props}>
+            <View >
+                <Text style={styles.title}>VaultDoc</Text>
+                <Text style={styles.subtitle}>the new way to securely stores your documents 🔒</Text>
+            </View>
             <DrawerItemList {...props} />
         </DrawerContentScrollView>
     );
 }
 
 export default Drawer;
+
+const styles = StyleSheet.create({
+    title: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        alignItems: 'center',
+        fontWeight: 'bold',
+    },
+    subtitle: {
+        paddingHorizontal: 13,
+        paddingVertical: 5,
+        alignItems: 'center',
+        color: 'grey',
+        fontSize: 12,
+    },
+});
