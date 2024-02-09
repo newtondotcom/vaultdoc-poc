0aa817390911ef777a6fd2f7e554b868f5a7c3d7
newtondotcom <asphalt9fr@gmail.com>
2024-02-09 23:20:42 +0100

    Revert "share button for pdf viewing"
    This reverts commit 5d92b030472bb762de42222f4dc255374903c12b.

--- src/screens/Document.tsx
+++ src/screens/Document.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import { View, Text, Dimensions, StyleSheet, Pressable } from 'react-native';
 import Pdf from 'react-native-pdf';
-import { sharePDF } from '../ui/Share';
-import mockData from '../utils/mock';
+import { Share } from "react-native";
 
 const Document = () => {
-  //const pdf = "https://web.wpi.edu/Images/CMS/Provost/landscape.pdf";
-  const source = { uri: 'https://web.wpi.edu/Images/CMS/Provost/landscape.pdf', cache: true };
+  
+  const source = { uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf', cache: true };
   //const source = require('./test.pdf');  // ios only
   //const source = {uri:'bundle-assets://test.pdf' };
   //const source = {uri:'file:///sdcard/test.pdf'};
-  //const source = {uri:"data:application/pdf;base64,"+mockData.document};
+  //const source = {uri:"data:application/pdf;base64,JVBERi0xLjcKJc..."};
   //const source = {uri:"content://com.example.blobs/xxxxxxxx-...?offset=0&size=xxx"};
   //const source = {uri:"blob:xxxxxxxx-...?offset=0&size=xxx"};
 
 
-  //const url = "data:application/pdf;base64,JVBERi0xLjIgDQo5IDAgb2JqDQo8PA0KPj4NCnN0cmVhbQ0KQlQvIDMyIFRmKCAgSGVsbG8gV29ybGQgICApJyBFVA0KZW5kc3RyZWFtDQplbmRvYmoNCjQgMCBvYmoNCjw8DQovVHlwZSAvUGFnZQ0KL1BhcmVudCA1IDAgUg0KL0NvbnRlbnRzIDkgMCBSDQo+Pg0KZW5kb2JqDQo1IDAgb2JqDQo8PA0KL0tpZHMgWzQgMCBSIF0NCi9Db3VudCAxDQovVHlwZSAvUGFnZXMNCi9NZWRpYUJveCBbIDAgMCAyNTAgNTAgXQ0KPj4NCmVuZG9iag0KMyAwIG9iag0KPDwNCi9QYWdlcyA1IDAgUg0KL1R5cGUgL0NhdGFsb2cNCj4+DQplbmRvYmoNCnRyYWlsZXINCjw8DQovUm9vdCAzIDAgUg0KPj4NCiUlRU9G";
-  //const urlb64 = "JVBERi0xLjIgDQo5IDAgb2JqDQo8PA0KPj4NCnN0cmVhbQ0KQlQvIDMyIFRmKCAgSGVsbG8gV29ybGQgICApJyBFVA0KZW5kc3RyZWFtDQplbmRvYmoNCjQgMCBvYmoNCjw8DQovVHlwZSAvUGFnZQ0KL1BhcmVudCA1IDAgUg0KL0NvbnRlbnRzIDkgMCBSDQo+Pg0KZW5kb2JqDQo1IDAgb2JqDQo8PA0KL0tpZHMgWzQgMCBSIF0NCi9Db3VudCAxDQovVHlwZSAvUGFnZXMNCi9NZWRpYUJveCBbIDAgMCAyNTAgNTAgXQ0KPj4NCmVuZG9iag0KMyAwIG9iag0KPDwNCi9QYWdlcyA1IDAgUg0KL1R5cGUgL0NhdGFsb2cNCj4+DQplbmRvYmoNCnRyYWlsZXINCjw8DQovUm9vdCAzIDAgUg0KPj4NCiUlRU9G"; 
+  const url = "data:application/pdf;base64,JVBERi0xLjQKJcfsj6IKMjYgM...";
   return (
       <View style={styles.container}>
           <Pdf
@@ -35,9 +33,8 @@ const Document = () => {
               }}
               trustAllCerts={false}
               style={styles.pdf}/>
-            <Pressable 
-                style={{marginBottom: 120}}
-            onPress={async () => sharePDF(mockData.document)}>
+            <Pressable onPress={async () => 
+await Share.share({ url })}>
               <Text>Share</Text>
             </Pressable>
       </View>
