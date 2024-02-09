0134d0b76e86c6be13a02941cf731d38cc102056
newtondotcom <asphalt9fr@gmail.com>
2024-02-09 23:45:02 +0100

    revert share to make it work

--- src/screens/Document.tsx
+++ src/screens/Document.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
-import { View, Text, Dimensions, StyleSheet, Pressable } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, Pressable, ShareContent } from 'react-native';
 import Pdf from 'react-native-pdf';
 import { Share } from "react-native";
+import mockData from '../utils/mock';
 
 const Document = () => {
+
+    const sharePDF = async () => {
+        try {
+            let content : ShareContent = {
+                title: "Title", // android only
+                message: "Your pdf is here!",
+                url: "data:application/pdf;base64,"+mockData.document, // ios only
+            };
+            let options : ShareOptions = {
+                dialogTitle: "Share PDF", // android only
+                excludedActivityTypes: [
+                    //"com.apple.UIKit.activity.PostToTwitter"
+                ], // ios only
+                tintColor: "green", // ios only
+                subject: "Subject" // ios only for email
+            };
+            const result = await Share.share(content, options);
+            if (result.action === Share.sharedAction) {
+                if (result.activityType) {
+                    // shared with activity type of result.activityType
+                } else {
+                    // shared
+                }
+            } else if (result.action === Share.dismissedAction) {
+                // dismissed
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
   
-  const source = { uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf', cache: true };
+    const source = { uri: 'https://web.wpi.edu/Images/CMS/Provost/landscape.pdf', cache: true };
   //const source = require('./test.pdf');  // ios only
   //const source = {uri:'bundle-assets://test.pdf' };
   //const source = {uri:'file:///sdcard/test.pdf'};
@@ -33,8 +64,7 @@ const Document = () => {
               }}
               trustAllCerts={false}
               style={styles.pdf}/>
-            <Pressable onPress={async () => 
-await Share.share({ url })}>
+            <Pressable style={{paddingBottom:50}} onPress={sharePDF}>
               <Text>Share</Text>
             </Pressable>
       </View>
